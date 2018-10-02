5c7c17134ed06aec2f31af4d724d533c1e81b845
Nathan Mkaya <nathanmkaya@gmail.com>
2018-10-02 16:28:21 +0300

    Initial commit

---
+++ app/src/main/java/mobi/devs/codebuildshipke/MainActivity.kt
@@ -0,0 +1,12 @@
+package mobi.devs.codebuildshipke
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

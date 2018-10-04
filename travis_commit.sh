b934b279a18217293856818fc6ad6bc129fbaf42
Michael Bukachi <michaelbukachi@gmail.com>
2018-10-04 14:05:04 +0300

    Modified travis commit script

--- travis_commit.sh
+++ travis_commit.sh
@@ -45,6 +45,7 @@ function travis-branch-commit() {
 #        msg "not pushing updates to branch $TRAVIS_BRANCH"
 #        return 0
 #    fi
+
     if ! git push --quiet --follow-tags "$remote" "$TRAVIS_BRANCH" > /dev/null 2>&1; then
         err "failed to push git changes"
         return 1
